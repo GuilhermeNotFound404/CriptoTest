c830e59b4d9c685af03ce743f0e64d73e0ceb8c0
GuilhermeNotFound404 <leonadokof123@gmail.com>
2022-10-03 14:33:20 -0300

    IPSSI

---
+++ Criptografia/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Criptografia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnLimpar = new System.Windows.Forms.Button();
+            this.BtnCriptografar = new System.Windows.Forms.Button();
+            this.TxtDesceiptografado = new System.Windows.Forms.TextBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.TxtCriptografado = new System.Windows.Forms.TextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.TxtCriptografar = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnLimpar
+            // 
+            this.BtnLimpar.Location = new System.Drawing.Point(93, 179);
+            this.BtnLimpar.Name = "BtnLimpar";
+            this.BtnLimpar.Size = new System.Drawing.Size(78, 23);
+            this.BtnLimpar.TabIndex = 23;
+            this.BtnLimpar.Text = "Limpar";
+            this.BtnLimpar.UseVisualStyleBackColor = true;
+            this.BtnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // BtnCriptografar
+            // 
+            this.BtnCriptografar.Location = new System.Drawing.Point(15, 179);
+            this.BtnCriptografar.Name = "BtnCriptografar";
+            this.BtnCriptografar.Size = new System.Drawing.Size(72, 23);
+            this.BtnCriptografar.TabIndex = 22;
+            this.BtnCriptografar.Text = "Criptografar";
+            this.BtnCriptografar.UseVisualStyleBackColor = true;
+            this.BtnCriptografar.Click += new System.EventHandler(this.BtnCriptografar_Click);
+            // 
+            // TxtDesceiptografado
+            // 
+            this.TxtDesceiptografado.Location = new System.Drawing.Point(15, 137);
+            this.TxtDesceiptografado.Name = "TxtDesceiptografado";
+            this.TxtDesceiptografado.Size = new System.Drawing.Size(156, 20);
+            this.TxtDesceiptografado.TabIndex = 21;
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(12, 121);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(122, 13);
+            this.Label3.TabIndex = 20;
+            this.Label3.Text = "Texto descriptografado: ";
+            // 
+            // TxtCriptografado
+            // 
+            this.TxtCriptografado.Location = new System.Drawing.Point(15, 89);
+            this.TxtCriptografado.Name = "TxtCriptografado";
+            this.TxtCriptografado.Size = new System.Drawing.Size(156, 20);
+            this.TxtCriptografado.TabIndex = 19;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(12, 73);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(105, 13);
+            this.Label2.TabIndex = 18;
+            this.Label2.Text = "Texto criptografado: ";
+            // 
+            // TxtCriptografar
+            // 
+            this.TxtCriptografar.Location = new System.Drawing.Point(15, 40);
+            this.TxtCriptografar.Name = "TxtCriptografar";
+            this.TxtCriptografar.Size = new System.Drawing.Size(156, 20);
+            this.TxtCriptografar.TabIndex = 17;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(12, 24);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(120, 13);
+            this.Label1.TabIndex = 16;
+            this.Label1.Text = "Texto para criptografar: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 448);
+            this.Controls.Add(this.BtnLimpar);
+            this.Controls.Add(this.BtnCriptografar);
+            this.Controls.Add(this.TxtDesceiptografado);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.TxtCriptografado);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.TxtCriptografar);
+            this.Controls.Add(this.Label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button BtnLimpar;
+        internal System.Windows.Forms.Button BtnCriptografar;
+        internal System.Windows.Forms.TextBox TxtDesceiptografado;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.TextBox TxtCriptografado;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.TextBox TxtCriptografar;
+        internal System.Windows.Forms.Label Label1;
+    }
+}
+
---
+++ Criptografia/Form1.cs
@@ -0,0 +1,112 @@
+﻿            using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+            namespace Criptografia
+            {
+                public partial class Form1 : Form
+                {
+
+                    string chave = "eliasnechar";
+                    byte[] salto = new byte[]{0x6e, 0x49, 0x61, 0x76, 0x20, 0x4d,
+                                              0x76, 0x65, 0x64, 0x65, 0x76, 0x65,
+                                              0x64 };
+                    public Form1()
+                    {
+                        InitializeComponent();
+                    }
+
+                    private void BtnLimpar_Click(object sender, EventArgs e)
+                    {
+                        TxtCriptografado.Text = String.Empty;
+                        TxtCriptografar.Text = String.Empty;
+                        TxtDesceiptografado.Text = String.Empty;
+
+                    }
+
+                    private void BtnCriptografar_Click(object sender, EventArgs e)
+                    {
+                        if (string.IsNullOrWhiteSpace(TxtCriptografar.Text))
+                        {
+                            MessageBox.Show("Informe o texto para ser criptografado.","Criptografar texto", MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                            TxtCriptografar.Focus();
+                            return;
+                        }
+
+                        TxtCriptografado.Text = Criptografar(TxtCriptografar.Text, chave, salto);
+                        TxtDesceiptografado.Text = Descriptografar(TxtCriptografado.Text, chave, salto);
+                     }
+
+                    public string Criptografar(string texto, string chave, byte[] salto) 
+                    {
+
+                        byte[] bytesBuff = Encoding.Unicode.GetBytes(texto);
+                        using (Aes aes = Aes.Create())
+                        {
+                            Rfc2898DeriveBytes cripto = new Rfc2898DeriveBytes(chave, salto);
+
+                            aes.Key = cripto.GetBytes(32);
+                            aes.IV = cripto.GetBytes(16);
+
+                            using (MemoryStream mStream = new MemoryStream())
+                            {
+                                using (CryptoStream cStream = new CryptoStream(mStream,
+                                    aes.CreateEncryptor(),
+                                    CryptoStreamMode.Write))
+                                {
+                                    cStream.Write(bytesBuff, 0, bytesBuff.Length);
+                                    cStream.Close();
+                                }
+
+                                texto = Convert.ToBase64String(mStream.ToArray());
+
+                            }
+                        }
+                        return texto;
+                    }
+
+
+                    public string Descriptografar(string texto, string chave, byte[] salto)
+                    {
+                        texto = texto.Replace(" ", "+");
+                        byte[] bytesBuff = Convert.FromBase64String(texto);
+
+                        using (Aes aes = Aes.Create())
+                        {
+                            Rfc2898DeriveBytes cripto = new Rfc2898DeriveBytes(chave, salto);
+
+                            aes.Key = cripto.GetBytes(32);
+                            aes.IV = cripto.GetBytes(16);
+
+                            using (MemoryStream mStream = new MemoryStream())
+                            {
+                                using (CryptoStream cStream = new CryptoStream(mStream,
+                                                                               aes.CreateDecryptor(),
+                                                                               CryptoStreamMode.Write))
+                                {
+                                    cStream.Write(bytesBuff, 0, bytesBuff.Length);
+                                    cStream.Close();
+                                }
+
+                                texto = Encoding.Unicode.GetString(mStream.ToArray());
+                            }
+                        }
+                        return texto;
+                    }
+     
+
+                    private void Form1_Load(object sender, EventArgs e)
+                    {
+                      
+
+                    }
+                }
+            }
